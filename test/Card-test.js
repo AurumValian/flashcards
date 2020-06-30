d0ddd2b9ee38abe913d8a468195684cda25c9111
Ben McClung <aurumvalian@gmail.com>
2020-06-29 18:22:45 -0600

    Change expect to should in Card-test.js

--- test/Card-test.js
+++ test/Card-test.js
@@ -1,5 +1,5 @@
 const chai = require('chai');
-const expect = chai.expect;
+var should = require('chai').should();
 
 const Card = require('../src/Card');
 
@@ -7,26 +7,26 @@ describe('Card', function() {
 
   it.skip('should be a function', function() {
     const card = new Card();
-    expect(Card).to.be.a('function');
+    (Card).should.be.a('function');
   });
 
   it.skip('should be an instance of Card', function() {
     const card = new Card();
-    expect(card).to.be.an.instanceof(Card);
+    (card).should.be.an.instanceof(Card);
   }); 
 
   it.skip('should store a question', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
+    (card.question).should.equal('What allows you to define a set of related information using key-value pairs?');
   });  
 
   it.skip('should store a list of possible answers', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    expect(card.answers).to.deep.equal(['object', 'array', 'function']);
+    (card.answers).should.deep.equal(['object', 'array', 'function']);
   });  
 
   it.skip('should store the correct answer', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    expect(card.correctAnswer).to.equal('object');
+    (card.correctAnswer).should.equal('object');
   });
 });
