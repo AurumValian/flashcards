3991532e1d1fc5bcad86604a9f1b193e064e4fe4
Ben McClung <aurumvalian@gmail.com>
2020-06-30 21:06:05 -0600

    Add countCards test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -51,4 +51,26 @@ describe('Deck', function() {
     }])
   })
 
+  it('should be able to count the number of cards in the deck', function() {
+    let cards = [{
+      "id": 3,
+      "question": "What type of prototype method directly modifies the existing array?",
+      "answerChoices": ["mutator method", "accessor method", "iteration method"],
+      "correctAnswer": "mutator method"
+    }, {
+      "id": 4,
+      "question": "What type of prototype method does not modify the existing array but returns a particular representation of the array?",
+      "answerChoices": ["mutator method", "accessor method", "iteration method"],
+      "correctAnswer": "accessor method"
+    }, {
+      "id": 5,
+      "question": "What type of prototype method loops through the existing array and applies a callback function that may mutate each element and return a new value?",
+      "answerChoices": ["mutator method", "accessor method", "iteration method"],
+      "correctAnswer": "iteration method"
+    }];
+    let deck = new Deck(cards);
+    let cardCount = deck.countCards();
+    (cardCount).should.equal(3);
+  })
+
 })
